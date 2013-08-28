4762eeefd440b2fa9bd57ff191fcdb6d98ae5090
Chris Mailloux <chrismayu@gmail.com>
2013-08-28 12:18:08 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeChurchApp::Application.config.secret_token = '2ee15cd34038a0bf3849840bf4732b604cd4d70f1ad9d0fb0d4f9dd22655311280b9121240d9f1a2c470ab30081f0c9143a969a76c69ffe8b46d88f93adff4d2'
