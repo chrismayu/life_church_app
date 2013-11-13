226eca316096b2b3f9ab0e400910176312ae3139
Chris Mailloux <chrismayu@gmail.com>
2013-11-13 17:28:43 -0500

    added valuations

--- app/models/service_time.rb
+++ app/models/service_time.rb
@@ -1,5 +1,8 @@
 class ServiceTime < ActiveRecord::Base
   attr_accessible :day, :end_time, :service_name, :start_time
   
+  
+  validates :service_name, presence: true 
+  
    default_scope order: 'service_times.day ASC'
 end
