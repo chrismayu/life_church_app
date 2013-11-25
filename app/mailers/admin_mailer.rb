f8315862ef0d6be04f5f71d097b3ace4fb8d107e
Chris Mailloux <chrismayu@gmail.com>
2013-11-25 15:49:02 -0500

    admin mailer - added @member

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -9,6 +9,7 @@ class AdminMailer < ActionMailer::Base
   #   en.admin_mailer.new_member_signed_up.subject
   #
   def new_user_waiting_for_approval(member)
+    @member = member
     mail(:subject => "#{member.name} - Approval Needed")
   end
 end
