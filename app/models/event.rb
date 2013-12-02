251046223e9447184148836a98c5d8264c066836
Chris Mailloux <chrismayu@gmail.com>
2013-12-02 16:52:58 -0500

    Added Date check for Events page

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,13 @@ class Event < ActiveRecord::Base
 has_many :event_pictures, :foreign_key => :event_id, :primary_key =>  :id, :inverse_of => :event, :dependent => :destroy
 
 
+validates  :event_name, :presence => true
+validates  :event_date, :presence => true
+validates  :remove_event_date, :presence => true
+ 
+validates_datetime :event_date
+validates_datetime :remove_event_date, :after => :event_date, :after_message => 'must be before "Start Time"'
+
+
 
 end
