ae9d519e7d644549b661bc575855bf9054a160ae
Chris Mailloux <chrismayu@gmail.com>
2013-12-11 13:25:04 -0500

    added titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
+#encoding: utf-8
 module ApplicationHelper
 
- 
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title)                          # Method definition
+    base_title = "Life Church International"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{page_title}"                 # String interpolation
+    end
+  end
 
 
   def display_base_errors resource
