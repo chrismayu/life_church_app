6e999939a86360bc2f232e17b73d5c37bd434e99
Chris Mailloux <chrismayu@gmail.com>
2013-12-21 18:47:57 -0500

    finsihed alerts

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,2 +1,47 @@
 module AnnouncementsHelper
+  
+  def display_status_of_announcement(announcement) 
+    
+    now = Time.zone.now
+    
+    if announcement.starts_at <= now && announcement.ends_at >= now 
+      
+      return content_tag(:div, content_tag(:span, "Active"), class: "label label-success")  
+      
+    else
+      
+      if announcement.starts_at >= now  
+        return content_tag(:div, content_tag(:span, "Pending"), class: "label label-info")  
+      end
+      
+      if announcement.ends_at <= now  
+        return content_tag(:div, content_tag(:span, "Expired"), class: "label label-important")  
+      end
+      
+    end
+
+  end
+  
+  
+  def alert_colour(announcement)
+    
+    
+    case announcement.colours
+      when 'success'
+        return content_tag(:div, content_tag(:span, "Green"), class: "label label-success") 
+      when 'error'
+        return content_tag(:div, content_tag(:span, "Red"), class: "label label-important") 
+      when 'info'
+        return content_tag(:div, content_tag(:span, "Blue"), class: "label label-info") 
+      when 'warning'
+        return content_tag(:div, content_tag(:span, "Yellow"), class: "label label-warning") 
+    end
+    
+    
+    
+  end  
+  
+  
+  
+ 
 end
