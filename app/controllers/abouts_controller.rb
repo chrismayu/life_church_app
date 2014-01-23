68398436982d501cf50e27f2e690a6db3be84a6e
Chris Mailloux <chrismayu@gmail.com>
2014-01-23 12:18:23 -0500

    Remove extra word in Controller

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -12,7 +12,7 @@ class AboutsController < ApplicationController
      events_side_forced = Event.where(:display_main_page => true).where(:force_on_main_page => true)
      
      if events_side_forced.count >= 3
-        @events_side = events_side_forced    :highlight
+        @events_side = events_side_forced   
      else
         @events_side = Event.where(:display_main_page => true).where("remove_event_date >= ?", @today).order("highlight DESC").order("event_type DESC").order("force_on_main_page DESC").limit(3).shuffle 
      end
