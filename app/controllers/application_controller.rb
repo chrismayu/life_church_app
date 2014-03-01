4bbe3450cb377ba5529995dcbdedd4c223177af8
Chris Mailloux <chrismayu@gmail.com>
2014-02-28 19:25:18 -0500

    Added Trackable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   
   
   
-  before_filter :set_ministries, :setup_about_info, :setup_site_setup_info
+  before_filter :set_ministries, :setup_about_info, :setup_site_setup_info, :update_last_sign_in_at
   def set_ministries
     @ministries_menu = Ministry.all
   end
@@ -21,6 +21,18 @@ class ApplicationController < ActionController::Base
   def setup_site_setup_info
     @site_setup_menu = SiteSetup.last
   end  
+  
+  
+  
+
+  protected
+
+  def update_last_sign_in_at
+    if user_signed_in? && !session[:logged_signin]
+      sign_in(current_user, :force => true)
+      session[:logged_signin] = true
+    end
+  end
   
   
 end
