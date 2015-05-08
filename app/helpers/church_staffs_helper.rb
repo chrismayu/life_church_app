a12a3b107644dcf9f795d6830f4f8afc8da6af98
chrismayu <chrismayu@gmail.com>
2015-05-07 22:26:46 -0400

    added nil check

--- app/helpers/church_staffs_helper.rb
+++ app/helpers/church_staffs_helper.rb
@@ -40,84 +40,86 @@ module ChurchStaffsHelper
   end
   
   def url(staff)
-    
+     unless staff.url !=nil
 	    unless staff.url.blank?
         content_tag(:p, content_tag(:i," ", class: "icon-globe") + link_to(" Blog", staff.url) )
       end
-      			
+      end			
   end
   
   def birthday(staff)
-    
+    unless staff.birthday !=nil
 	    unless staff.birthday.blank?
         content_tag(:p, content_tag(:i," ", class: "icon-gift") + staff.birthday.strftime(" %B #{ staff.birthday.day.ordinalize}") )
       end
-      			
+    end			
   end
  
  
  
   def facebook(staff)
-    
+    unless staff.facebook !=nil
 	    unless staff.facebook.blank?
         content_tag(:p, content_tag(:i," ", class: "icon-facebook") + link_to(" Facebook", staff.facebook))
       end
-      			
+    end  			
   end
   
   
   def linkedin(staff)
-    
+    unless staff.linked_in !=nil
 	    unless staff.linked_in.blank?
         content_tag(:p, content_tag(:i," ", class: "icon-linkedin") + link_to(" Linkedin", staff.linked_in))
        end
-  
+     end
  end
    
   
   def twitter(staff)
-    
+    unless staff.twitter !=nil
 	    unless staff.twitter.blank?
         content_tag(:p, content_tag(:i," ", class: "icon-twitter") + link_to(" Twitter", staff.twitter))  			
       end
-  
+    end
 end 
   
   def google_plus(staff)
-    
+    unless staff.google_plus !=nil
 	    unless staff.google_plus.blank?
         content_tag(:p, content_tag(:i," ", class: "icon-google-plus") + link_to(" Google Plus", staff.google_plus))  			
       end
-  
+    end
 end 
   
   
   
   def spouse(staff)
-    
+    unless staff.spouse_first_name !=nil
 		  unless staff.spouse_first_name.blank? 
      content_tag(:p, content_tag(:i," ", class: "icon-smile") + " #{staff.spouse_first_name.capitalize}")
-      end   
+      end 
+    end  
  end
   
  def length_service(staff)
    
-  
+  unless staff.started_service !=nil
     unless staff.started_service.blank?  	
         if staff.started_service?  
        "Length of Service:" + time_ago_in_words(staff.started_service.to_formatted_s(:long) )    
         end    
-   end    
+   end 
+ end   
   
  end
   
   def birth_location(staff)
   
-  
+  unless staff.birth_location !=nil
      unless staff.birth_location.blank?
      content_tag(:p, content_tag(:i," ", class: "icon-icon-map-marker") + "  #{staff.birth_location.capitalize}" + tag(:br))
     end 
- 
+  end
     
     
   end
