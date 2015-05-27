4d3506515f5575e102602f6007d3e4869ae5809d
chrismayu <chrismayu@gmail.com>
2015-05-27 19:51:14 -0400

    added validations

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -2,4 +2,10 @@ class Faq < ActiveRecord::Base
   attr_accessible :admin, :answer, :member_only, :title, :user_id, :the_answer, :editor
   has_many :answers
   
+  
+  validates :the_answer, presence: true
+  validates :title, presence: true, length: { maximum: 240 }
+  
+  
+  
 end
