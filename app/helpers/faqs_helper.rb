f2d20110366c29114edb9df087bd5d76f17879d3
chrismayu <chrismayu@gmail.com>
2015-05-27 16:20:57 -0400

    added faqs helper

--- app/helpers/faqs_helper.rb
+++ app/helpers/faqs_helper.rb
@@ -1,5 +1,55 @@
 module FaqsHelper
   
+  
+  
+  
+      def faq_label(faq, member = false)
+        
+        
+         if member == false
+         
+         
+        if faq.member_only?
+  
+          html = <<-HTML
+          <span class="label"> <i class="icon-lock"></i>Member's Only</span>
+          HTML
+        else 
+          
+           html = ""
+         
+         end
+  
+          #  html = <<-HTML
+          #  <span class="label label-success"> <i class="icon-unlock"></i>Member's Only</span>
+           # HTML
+            
+ 
+        else
+          if faq.member_only?
+  
+            html = <<-HTML
+            <span class="label label-success"> <i class="icon-unlock"></i>Member's Only</span>
+            HTML
+          else 
+          
+             html = ""
+           end  
+        
+        end   
+ 
+        html.html_safe
+      end
+      
+      
+      
+  
+      
+  
+   
+ 
+  
+  
   def faq_markdown(text)
     
     if text !=nil 
